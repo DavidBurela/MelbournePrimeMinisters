7edb33c82d93a619a55f72e8b529d3952cc37d20
David Burela <David.Burela@gmail.com>
2015-09-17 14:18:22 +1000

    Created the Model, controllers, and views
    Created a way to create, edit and view Prime Ministers

---
+++ MelbournePrimeMinisters/Models/MelbournePrimeMinistersContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace MelbournePrimeMinisters.Models
+{
+    public class MelbournePrimeMinistersContext : DbContext
+    {
+        public MelbournePrimeMinistersContext() : base("name=MelbournePrimeMinistersContext")
+        {
+        }
+
+        public DbSet<PoliticalParty> PoliticalParties { get; set; }
+
+        public DbSet<PrimeMinister> PrimeMinisters { get; set; }
+    }
+}
---
+++ MelbournePrimeMinisters/Models/PrimeMinister.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelbournePrimeMinisters.Models
+{
+    public class PrimeMinister
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+        [ForeignKey("PoliticalParty")]
+        public int PoliticalPartyId { get; set; }
+        public PoliticalParty PoliticalParty { get; set; }
+    }
+
+    public class PoliticalParty
+    {
+        public int Id { get; set; }
+        public string PartyName { get; set; }
+    }
+}
